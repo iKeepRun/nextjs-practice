fd7c27434fa09ce27d9c74a74f538bdf53f011fe
iKeepRun <592012072@qq.com>
2025-10-27 22:24:23 +0800

    主页开发，标题和按钮添加

---
+++ app/components/ExploreBtn.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const ExploreBtn = () => {
+    return (
+        <button>ExploreBtn</button>
+    )
+}
+export default ExploreBtn
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+import ExploreBtn from "@/app/components/ExploreBtn";
+
 const Home = () => {
     return (
-        <h1>welcome to next.js 16</h1>
+        <section>
+            <h1 className="text-center">The Hub for Every Dev <br/> Event you Can&apos;t Miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups, and Conferences,All in One Place ExploreBtn </p>
+
+            <ExploreBtn />
+        </section>
     )
 }
 export default Home
